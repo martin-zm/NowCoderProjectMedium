8cba294dc44b6d07d12ecb8ac9a1765eec15b3df
zm <15071239679@163.com>
2017-10-07 19:18:46 +0800

    change directory name

--- toutiao/src/main/java/com/nowcoder/ToutiaoApplication.java
+++ toutiao/src/main/java/com/nowcoder/ToutiaoApplication.java
@@ -2,11 +2,19 @@ package com.nowcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootApplication
-public class ToutiaoApplication {
+public class ToutiaoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ToutiaoApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return super.configure(builder);
 	}
 }
